df0599eca2d4e8c279c2c7c357d19ec120a61b57
chris <c.giacofei@gmail.com>
2013-11-21 10:38:02 -0500

    Completed script to call sorttv when torrents complete (still not tested to see if it works). Removed sort directories from sorttv.conf so they must be specified at the command line.

--- scripts/torrent-complete.sh
+++ scripts/torrent-complete.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
+# Final locations for downloaded files
+TVDIR = "/media/sf_Iomega_HDD/TV Shows"
+PREMDIR = "/media/sf_Iomega_HDD/TV Premieres"
+MOVIEDIR = "/media/sf_Iomega_HDD/Movies"
+
+# Transmission download folders
+TVTOR = "tv_shows"
+PREMTOR = "tv_premeries"
+MOVIETOR = "movies"
+
+# Path to SortTV script
+SORTTV = /home/chris/.flexget/scripts/sorttv/sorttv.pl
+
+LOGFILE = /home/chris/logs/torrent_complete.log
+touch $LOGFILE
+
 DIR = basename $TR_TORRENT_DIR 
 
 # TV Shows
-if [ DIR = "tv_shows" ]; then
-    perl sorttv/sorttv.pl $TR_TORRENT_DIR  /media/sf_Iomega_HDD/TV Shows
-fi
+if [ DIR = $TVTOR ]; then
+    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+    echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/TV Shows" >$LOGFILE
+    perl $SORTTV $TR_TORRENT_DIR  $TVDIR
 
 # TV Premieres
-if [ DIR = "tv_premeries" ]; then
-    perl sorttv/sorttv.pl $TR_TORRENT_DIR  /media/sf_Iomega_HDD/TV Premieres
-fi
+elif [ DIR = $PREMTOR ]; then
+    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+    echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/TV Premieres" >$LOGFILE
+    perl $SORTTV $TR_TORRENT_DIR  $PREMDIR
 
 # Movies
-if [ DIR = "movies" ]; then
-    perl sorttv/sorttv.pl $TR_TORRENT_DIR  /media/sf_Iomega_HDD/Movies
+elif [ DIR = $MOVIETOR ]; then
+    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+    echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/Movies" >$LOGFILE
+    perl $SORTTV $TR_TORRENT_DIR  $MOVIEDIR
+
+# Something went wrong or not a media file
+else
+    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+    echo "Error attempting to move from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/Movies" >$LOGFILE
+    echo "Is this a media file?" >$LOGFILE
 fi
