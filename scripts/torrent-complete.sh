a41b7455160d7e0c872d1184d0638d83d8c778b4
chris <c.giacofei@gmail.com>
2013-12-03 08:01:35 -0500

    Fixed and incorrect variable call in transmission bash script. Modified the log output to remove redundency.

--- scripts/torrent-complete.sh
+++ scripts/torrent-complete.sh
@@ -18,27 +18,25 @@ touch $LOGFILE
 
 DIR = basename $TR_TORRENT_DIR 
 
+echo "$TR_TIME_LOCALTIME --- Transmission finished downloading \"$TR_TORRENT_NAME\", sorting..." >$LOGFILE
+
 # TV Shows
-if [ DIR = $TVTOR ]; then
-    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+if [ $DIR = $TVTOR ]; then
     echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/TV Shows" >$LOGFILE
     perl $SORTTV $TR_TORRENT_DIR  $TVDIR
 
 # TV Premieres
-elif [ DIR = $PREMTOR ]; then
-    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+elif [ $DIR = $PREMTOR ]; then
     echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/TV Premieres" >$LOGFILE
     perl $SORTTV $TR_TORRENT_DIR  $PREMDIR
 
 # Movies
-elif [ DIR = $MOVIETOR ]; then
-    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
+elif [ $DIR = $MOVIETOR ]; then
     echo "Moving from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/Movies" >$LOGFILE
     perl $SORTTV $TR_TORRENT_DIR  $MOVIEDIR
 
 # Something went wrong or not a media file
 else
-    echo "Transmission finished downloading \"$TR_TORRENT_NAME\" on $TR_TIME_LOCALTIME" >$LOGFILE
     echo "Error attempting to move from $TR_TORRENT_DIR to /media/sf_Iomega_HDD/Movies" >$LOGFILE
     echo "Is this a media file?" >$LOGFILE
 fi
