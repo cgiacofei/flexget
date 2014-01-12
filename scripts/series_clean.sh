961a5924a10fb250535ee1c82e278039cadfb6a1
chris <c.giacofei@gmail.com>
2014-01-12 08:13:09 -0500

    Cleanup script now has correct file path to the Daily Show.

--- scripts/series_clean.sh
+++ scripts/series_clean.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 find "/media/media-drive/TV Shows/The Colbert Report"/Season*  -mtime +14 -type f -delete
-find "/media/media-drive/TV Shows/The Daily Show"/Season*  -mtime +14 -type f -delete
+find "/media/media-drive/TV Shows/The Daily Show with Jon Stewart"/Season*  -mtime +14 -type f -delete
 
 find "/media/media-drive/TV Shows" -type d -empty -delete
