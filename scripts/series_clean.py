64d9b76ddbe096a68c46a69b71f15b8d13ef3a7f
chris <c.giacofei@gmail.com>
2014-01-17 22:07:00 -0500

    Framework in place to delete file.  Need to modify to actually delete files.

--- scripts/series_clean.py
+++ scripts/series_clean.py
@@ -7,19 +7,24 @@ tv_root = "/media/media-drive/TV Shows"
 s_clean = {"The Colbert Report":7,
            "The Daily Show with Jon Stewart":7}
 
+test_dir = "/home/chris/testing"
+
 def delete_old_files(dirpath, max_age):
     max_age *= 86400 # convert days to seconds
     present = time.time()
     file_list = []
+    
     for root, dirs, files in os.walk(dirpath):
         for old_thing in files:
             file_name = os.path.join(root, old_thing)
+            
             try:
-                if (present - os.path.getmtime(file_name)) > max_age:
+                if (present - os.path.getmtime(file_name)) > max_age and "Season" in root:
                     file_list.append(file_name)
                     print(file_name, str((present - os.path.getmtime(file_name)) / 86400))
             except:
                 pass
 
-for show in s_clean.items:
-    delete_old_files(os.join(tv_root,show,"Season"), s_clean[show])
+for show in s_clean:
+    delete_old_files(os.path.join(test_dir, show), s_clean[show])
+
