b369342210711e4733569243e0930e1f5576b940
chris <c.giacofei@gmail.com>
2014-01-18 07:27:47 -0500

    1. Added os.remove to the main function. 2. Created docstring for the file.

--- scripts/series_clean.py
+++ scripts/series_clean.py
@@ -1,5 +1,11 @@
 #! /bin/python
 
+"""series_clean.py: Removes old files from the system.
+
+The dictionary s_clean contains the show folders to scan and how long to
+keep the files within those folders.
+"""
+
 import os
 import time
 
@@ -12,7 +18,6 @@ test_dir = "/home/chris/testing"
 def delete_old_files(dirpath, max_age):
     max_age *= 86400 # convert days to seconds
     present = time.time()
-    file_list = []
     
     for root, dirs, files in os.walk(dirpath):
         for old_thing in files:
@@ -20,11 +25,10 @@ def delete_old_files(dirpath, max_age):
             
             try:
                 if (present - os.path.getmtime(file_name)) > max_age and "Season" in root:
-                    file_list.append(file_name)
-                    print(file_name, str((present - os.path.getmtime(file_name)) / 86400))
+                    os.remove(file_name)
             except:
                 pass
 
 for show in s_clean:
-    delete_old_files(os.path.join(test_dir, show), s_clean[show])
+    delete_old_files(os.path.join(tv_root, show), s_clean[show])
 
