24b26785319a462918737789ca6369b3cd29591d
chris <c.giacofei@gmail.com>
2014-01-22 06:27:46 -0500

    1. Hard coded 'Season' is now an optional function argument to make the script more general 2. Double quotes are now single quotes

--- scripts/series_clean.py
+++ scripts/series_clean.py
@@ -9,13 +9,18 @@ keep the files within those folders.
 import os
 import time
 
-tv_root = "/media/media-drive/TV Shows"
-s_clean = {"The Colbert Report":7,
-           "The Daily Show with Jon Stewart":7}
+tv_root = '/media/media-drive/TV Shows'
+s_clean = {'The Colbert Report':7,
+           'The Daily Show with Jon Stewart':7}
 
-test_dir = "/home/chris/testing"
+torrent_root = '/home/media/torrents/completed'
+torrent_location = {'tv_shows':30,
+                    'tv_premieres':30,
+                    'movies':30}
 
-def delete_old_files(dirpath, max_age):
+test_dir = '/home/chris/testing'
+
+def delete_old_files(dirpath, max_age, extra=''):
     max_age *= 86400 # convert days to seconds
     present = time.time()
     
@@ -24,11 +29,14 @@ def delete_old_files(dirpath, max_age):
             file_name = os.path.join(root, old_thing)
             
             try:
-                if (present - os.path.getmtime(file_name)) > max_age and "Season" in root:
+                file_age = present - os.path.getmtime(file_name))
+                if (file_age > max_age and extra in root:
                     os.remove(file_name)
             except:
                 pass
 
 for show in s_clean:
-    delete_old_files(os.path.join(tv_root, show), s_clean[show])
-
+    delete_old_files(os.path.join(tv_root,show), s_clean[show], 'Season')
+    
+for folder in torrent_location:
+    delete_old_files(os.path.join(torrent_root,folder), torrent_location[folder], folder)
