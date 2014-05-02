0af8b1b313a441fcc376750075109768fa2bf01d
cgiacofei <c.giacofei@gmail.com>
2014-05-02 10:55:48 +0000

    series_clean.py edited online with Bitbucket Fix incorrect log file location

--- scripts/series_clean.py
+++ scripts/series_clean.py
@@ -40,7 +40,7 @@ def delete_old_files(dirpath, max_age, extra=''):
                 file_age = present - os.path.getmtime(file_name)
                 if file_age > max_age and extra in root:
                     os.remove(file_name)
-                    with open(log_name, "a") as myfile:
+                    with open(os.path.join(log_location,log_name), "a") as myfile:
                         output = '%s --- Age %03d days | Removing: %s\n' % (timestamp, int(file_age)/86400, file_name)
                         myfile.write(output)
                         myfile.write(' Age: ' + str(int(file_age)/86400) + ' days | Removing: ' + file_name + '\n')
