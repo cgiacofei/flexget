3f4d063cafa0e2fb18212fe28c329879b1a34930
chris <c.giacofei@gmail.com>
2014-12-04 06:27:48 -0500

    Fix un-escaped spaces in file paths

--- scripts/mult_sort.sh
+++ scripts/mult_sort.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Sorting TV Shows"
-perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/torrents/completed/tv_shows --tv-directory=/media/media-drive/TV Shows 
+perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/torrents/completed/tv_shows --tv-directory="/media/media-drive/TV Shows"
 
 echo "Sorting TV Premieres"
-perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/torrents/completed/tv_premieres --tv-directory=/media/media-drive/TV Premieres
+perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/torrents/completed/tv_premieres --tv-directory="/media/media-drive/TV Premieres"
 
 echo "Sorting Movies"
 perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/torrents/completed/movies --movie-directory=/media/media-drive/Movies
