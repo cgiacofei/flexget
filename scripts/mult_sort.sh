86a479362ac8e40acaa61824645532b5359be744
chris <c.giacofei@gmail.com>
2015-04-28 06:43:02 -0400

    remove ionice from script

--- scripts/mult_sort.sh
+++ scripts/mult_sort.sh
@@ -3,19 +3,19 @@
 lockfile -r 0 .lockfile
 
 echo "Sorting TV Shows"
-ionice -n7 nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/tv_shows --tv-directory="/media/usb/TV Shows"
+nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/tv_shows --tv-directory="/media/usb/TV Shows"
 
 echo "Sorting TV Premieres"
-ionice -n7 nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/tv_premieres --tv-directory="/media/usb/TV Premieres"
+nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/tv_premieres --tv-directory="/media/usb/TV Premieres"
 
 echo "Sorting Movies"
-ionice -n7 nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/movies --movie-directory="/media/usb/Movies"
+nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/movies --movie-directory="/media/usb/Movies"
 
 echo "Sorting Kids TV"
-ionice -n7 nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/andrea_tv --tv-directory="/media/usb/Kid TV"
+nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/andrea_tv --tv-directory="/media/usb/Kid TV"
 
 echo "Sorting Kids Movies"
-ionice -n7 nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/andrea_movies --movie-directory="/media/usb/Kid Movies"
+nice perl /home/media/sorttv/sorttv.pl --directory-to-sort=/home/media/completed_files/andrea_movies --movie-directory="/media/usb/Kid Movies"
 
 rm -f .lockfile
 
