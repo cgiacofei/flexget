47a3535436411f38037f1ca51afcc3fdfcd751af
chris <c.giacofei@gmail.com>
2015-04-28 13:48:12 +0100

    repository update script

---
+++ scripts/check_update.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+REMOTE='whatbox'
+BRANCH='master'
+VENV='/home/media/.flexget/venv'
+
+git fetch origin
+CHANGES=$(git diff-index --name-only $REMOTE/$BRANCH)
+
+if [ -n "$CHANGES" ]; then
+    echo "Update available"
+
+    # Perform git pull
+    git pull origin $BRANCH
+
+    # Reload flexget congig
+    source $VENV/bin/activate
+    flexget daemon reload
+else
+    echo "Everything up-to-date"
+fi
